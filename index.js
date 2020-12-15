104b24c420630b58ac56cc87f8593fa5c7f3dd0d
PerpetuumG <perpetuumg@mail.ru>
2020-12-15 11:54:11 +0200

    Commit_2

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 var $start = document.querySelector('#start')
 var $game = document.querySelector('#game')
 var $time = document.querySelector('#time')
+var $result = document.querySelector('#result')
+var $timeHeader = document.querySelector('#time-header')
+var $resultHeader = document.querySelector('#result-header')
+var $gameTime = document.querySelector('#game-time')
 
 var score = 0
 var isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
+
+function show($el) {
+    $el.classList.remove('hide')
+}
+
+function hide($el) {
+    $el.classList.add('hide')
+}
+
 
 function startGame() {
+    score = 0
+    setGameTime()
+    $gameTime.setAttribute('disabled', 'true')
     isGameStarted = true
     $game.style.backgroundColor = '#fff'
-    $start.classList.add('hide')
+    hide($start)
 
-    var interval = setInterval(function () {
+    var interval = setInterval(function() {
         var time = parseFloat($time.textContent)
 
-        if (time <=0){
+        if (time <= 0) {
             clearInterval(interval)
-           endGame()
-        }
-        else{
+            endGame()
+        } else {
             $time.textContent = (time - 0.1).toFixed(1)
         }
     }, 100)
@@ -28,12 +44,30 @@ function startGame() {
     renderBox()
 }
 
-function endGame(){
+function setGameScore() {
+    $result.textContent = score.toString()
+}
+
+function setGameTime() {
+    var time = +$gameTime.value
+    $time.textContent = time.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
+}
+
+function endGame() {
     isGameStarted = false
+    setGameScore()
+    $gameTime.removeAttribute('disabled')
+    show($start)
+    $game.innerHTML = ''
+    $game.style.backgroundColor = '#ccc'
+    hide($timeHeader)
+    show($resultHeader)
 }
 
 function handleBoxClick(event) {
-    if(isGameStarted){
+    if (!isGameStarted) {
         return
     }
 
@@ -60,6 +94,7 @@ function renderBox() {
     box.setAttribute('data-box', 'true')
 
     $game.insertAdjacentElement('afterbegin', box)
+
 }
 
 function getRandom(min, max) {
